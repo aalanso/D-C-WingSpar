aa1645693ec0d72a09be3e3793cb704a208abddf
Flavio <flaviocicero55@gmail.com>
2024-04-22 15:00:31 +0200

    Update E06G_WingOptimisation.py

--- E06G_WingOptimisation.py
+++ E06G_WingOptimisation.py
@@ -1,11 +1,12 @@
 import math
 import numpy
 
-z=0
-def stepFunction(z):
+R_A = 1154.926
+R_B = 2075/3
+R_C = 925/3
+R_D = 5.6*9.81
 
-
-
+def M(z): return ( (R_A) - (R_B - 0.75) - (R_C - 1.95) - (R_D - 2.25) )*z
 
 def ShearBuckling():
 
