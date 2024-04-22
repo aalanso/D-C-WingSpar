03949d3e468fbe3278011c489dac33f3b3539fcb
Flavio <flaviocicero55@gmail.com>
2024-04-22 15:41:54 +0200

    Update E06G_WingOptimisation.py

--- E06G_WingOptimisation.py
+++ E06G_WingOptimisation.py
@@ -1,21 +1,41 @@
 import math
 import numpy
+import matplotlib.pyplot as plt
 
-R_A = 1154.926
+M_A = 1108.394
+R_A = 1045.064
 R_B = 2075/3
 R_C = 925/3
 R_D = 5.6*9.81
 
-def M(z): return ( (R_A) - (R_B - 0.75) - (R_C - 1.95) - (R_D - 2.25) )*z
-
-def ShearBuckling():
-
-
-def Bending():
-
-
-def BoltSpacing():
+def M(z): 
+    if 0<=z<=750:
+        return (R_A*(z))/1000 - M_A
+    elif 750<z<=1950:
+        return (R_A*(z) - R_B*(z - 750))/1000 - M_A
+    elif 1950<z<=2250:
+        return (R_A*(z) - R_B*(z - 750) - R_C*(z - 1950))/1000 - M_A
 
 
-def interRivetBuckling();
+moment = []
+z = []
+
+def totalMoment():
+    for i in range(0,2250):
+        moment.append(M(i))
+        z.append(i)
+    fig, ax = plt.subplots()
+    ax.plot(z,moment)
+    plt.show()
+totalMoment()
+#def ShearBuckling():
+
+#stress = ( M(Z) * y ) / I_xx
+#def Bending():
+
+
+#def BoltSpacing():
+
+
+#def interRivetBuckling();
     
