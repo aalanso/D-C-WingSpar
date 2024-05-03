d65f74eacf16a75b86dc8bf461ffd1c6bb767ff2
Alan Solinski <70856484+aalanso@users.noreply.github.com>
2024-05-03 10:56:57 +0200

    added bolts and disabling the flange plates at the end

--- ummm.py
+++ ummm.py
@@ -1,5 +1,6 @@
 # import E06G_WingOptimisation as F
 import matplotlib.pyplot as plt
+import numpy as np
 bolt_tau_max = 311e6
 
 sigma_ult = 483e6
@@ -56,21 +57,24 @@ def mass(no_reinf, reinf_len, no_flange_len_mm, n_bolts):
     volume_sh = 4*18.5*1.5*2250 * (mm)**3
     # this overestimates wrt masses from reader by around 5%
     volume_tot = volume_f+volume_sh+volume_sv+volume_w
-    volume_tot += n_bolts * 0.00139
     for i in  range(no_reinf): volume_tot += 4*18.5*0.8*reinf_len[i] * (mm)**3
-    return volume_tot*rho
+    return volume_tot*rho + n_bolts * 0.00139
 
 #print(mass(1, [900]))
 #print(mass(1, [900]) * rho)
 
 # z in cm, reinf_lengths in cm as well
-def I_xx(z, n_reinf, reinf_lengths):
+def I_xx(z, n_reinf, reinf_lengths, no_flange_len_mm):
     I_res = 0
 
     I_web = 0.8 * (148.4 ** 3) / 12
     I_stringer_vertical = 4 * ((1.5 * (20 ** 3) / 12 + 1.5 * 20 * (64.2 ** 2)))
     I_stringer_h = ((18.5) * (1.5 ** 3) / 12 + (18.5) * (1.5) * ((74.2 - 0.75) ** 2)) * 4
-    I_flanges = 2 * (40 * (0.8 ** 3) / 12 + 40 * 0.8 * (74.6) ** 2)
+
+    if z < 2250-no_flange_len_mm:
+        I_flanges = 2 * (40 * (0.8 ** 3) / 12 + 40 * 0.8 * (74.6) ** 2)
+    else:
+        I_flanges = 0
 
     I_res = I_web + I_stringer_h + I_stringer_vertical + I_flanges
 
@@ -83,11 +87,15 @@ def I_xx(z, n_reinf, reinf_lengths):
 
 # print(I_xx(1000, 0, []))
 
-def Q_NA(z, n_reinf, reinf_lengths):
+def Q_NA(z, n_reinf, reinf_lengths, no_flange_len_mm):
     Q_web = 0.8 * 74.2 * 37.1
     Q_stringer_vertical = 2 * (20 * 1.5 * 64.2)
     Q_stringer_h = (18.5 * 1.5 * (74.2 - 0.75)) * 2
-    Q_flange = (40 * 0.8 * 74.6)
+
+    if z < 2250 - no_flange_len_mm:
+        Q_flange = (40 * 0.8 * 74.6)
+    else:
+        Q_flange = 0
 
     Q_res = Q_web + Q_stringer_h + Q_stringer_vertical + Q_flange
 
@@ -130,16 +138,18 @@ def get_shear_buckling(V_int, Q_mm3, I_xx_mm4):
 
 
 
-def eval_setup(n_reinf, reinf_lengths):
+def eval_setup(n_reinf, reinf_lengths, no_flange_len, n_bolts):
     tau_int = []
     sigma_bending = []
     I = []
     Q = []
+    tau_crit = []
 
     for z in range(0, 2250):
-        I_here = I_xx(z, n_reinf, reinf_lengths)
-        Q_here = Q_NA(z, n_reinf, reinf_lengths)
+        I_here = I_xx(z, n_reinf, reinf_lengths, no_flange_len)
+        Q_here = Q_NA(z, n_reinf, reinf_lengths, no_flange_len)
         tau_int.append(V(z) * Q_here * 1e6 / (I_here * 0.8))
+        tau_crit.append(get_shear_buckling(V(z), Q_here, I_here))
         sigma_bending.append(get_bending_stress(M(z), I_here))
         I.append(I_here)
         Q.append(Q_here)
@@ -149,12 +159,20 @@ def eval_setup(n_reinf, reinf_lengths):
     print("Max internal shear stress: {}".format(max(tau_int)))
 
     fig, ax = plt.subplots()
-    ax.plot(tau_int)
+    ax.plot(tau_int, label="int")
+    ax.plot(tau_crit, label="crit")
+    plt.legend()
+
     plt.ylabel("Tau Internal")
     plt.show()
 
+
+
+
     fig, ax = plt.subplots()
-    ax.plot(sigma_bending)
+    ax.plot(sigma_bending, label="sigma bending")
+    ax.plot(np.full((2250), sigma_y), label="sigma_y")
+    print(np.full((1, 2250), sigma_y))
     plt.ylabel("Sigma Bending")
     plt.show()
 
@@ -168,35 +186,35 @@ def eval_setup(n_reinf, reinf_lengths):
     plt.ylabel("Q_NA [mm3]")
     plt.show()
 
-eval_setup(0, [])
+    print(mass(n_reinf, reinf_lengths, no_flange_len, n_bolts))
 
 
-F_max_bolt = bolt_tau_max * 0.25*3.14159*(0.004**2)
-# commented away not true
-# qqq = 40.8*0.8*74.6
-# s_max_mm = 2 * F_max_bolt * I_xx(1, 0,[]) / (V(1) * qqq)
-print("F_max_bolt [N]: {}".format(F_max_bolt))
-#print("Q: {} [mm3]".format(qqq))
-print("I_xx [mm4]: {}".format(I_xx(1, 0,[])))
-print("V_int [N]: {}".format(V(1)))
+eval_setup(0, [], 1450, 60)
 
+#
+# F_max_bolt = bolt_tau_max * 0.25*3.14159*(0.004**2)
+# # commented away not true
+# print("F_max_bolt [N]: {}".format(F_max_bolt))
+# #print("Q: {} [mm3]".format(qqq))
+# print("I_xx [mm4]: {}".format(I_xx(1, 0,[])))
+# print("V_int [N]: {}".format(V(1)))
+#
+#
+# s_max_irb = 0.8 * ( ( -(0.9*2.1*E) / get_bending_stress(M(1), I_xx(1, 0, [])) )**0.5)
+# print("s_max_irb: [mm] {}".format(s_max_irb))
 
-s_max_irb = 0.8 * ( ( -(0.9*2.1*E) / get_bending_stress(M(1), I_xx(1, 0, [])) )**0.5)
-print("s_max_irb: [mm] {}".format(s_max_irb))
+# def get_normal_flow_spacing(z):
+#     sm = M(z)*75/I_xx(z, 0, [])
+#     print(sm)
+#     qs = M(z)*74.2*20/I_xx(z, 0, [])
+#     qp = M(z)*75*20/I_xx(z, 0, [])
+#     dq = qs - qp
+#     dq = 1e6 * dq
+#     F_max_bolt = bolt_tau_max * 0.25 * 3.14159 * (0.004 ** 2) # N
+#
+#     return F_max_bolt/dq
+#
+#
+# print(get_normal_flow_spacing(1))
 
-def get_normal_flow_spacing(z):
-    sm = M(z)*75/I_xx(z, 0, [])
-    print(sm)
-    qs = M(z)*74.2*20/I_xx(z, 0, [])
-    qp = M(z)*75*20/I_xx(z, 0, [])
-    dq = qs - qp
-    dq = 1e6 * dq
-    F_max_bolt = bolt_tau_max * 0.25 * 3.14159 * (0.004 ** 2) # N
-
-    return F_max_bolt/dq
-
-
-print(get_normal_flow_spacing(1))
-
-print(mass(0, [], 0, 0))
 
